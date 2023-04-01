a577ad8f2d9ab8a47d9f45768d33418436e19441
paulahakeem <paula.hakeem@yahoo.com>
2023-04-02 01:51:52 +0200

    nodejs_app_added

---
+++ Nodejs_Application/nodeapp/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1 style="color: #2196F3;">Fixed_Solution_APP by Paula 🚀</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Example app listening at http://localhost:${port}`);
+});
