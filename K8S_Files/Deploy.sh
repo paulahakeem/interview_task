bfcb3e06c171eff02fd19488a46674f883cef955
paulahakeem <paula.hakeem@yahoo.com>
2023-04-01 06:58:49 +0200

    add asible files

---
+++ K8S_Files/Deploy.sh
@@ -0,0 +1,9 @@
+kubectl apply -f Namespace.yml
+kubectl apply -f DaemonSet.yml
+kubectl apply -f LoadBalancer.yml
+kubectl apply -f PV.yml
+kubectl apply -f PVC.yml
+kubectl apply -f ServiceAccount.yml
+kubectl apply -f test.yml
+kubectl apply -f jenkins-deployment.yml
+
